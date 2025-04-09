4fc9c069c35e7f7e53282996e18185f12ff58310
mirzanahid <mirzasaikat3@gmail.com>
2025-04-09 08:50:39 +0600

    README.MD updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -11,9 +11,13 @@ export default {
   database_url: process.env.DATABASE_URL,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASSWORD,
-  jwt_access_token: process.env.JWT_ACCESS_SECRET,
   server_base_url: process.env.SERVER_BASE_URL,
-
+  //added by mirza nahid
+  //jwt and refresh secret and expiry added here
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_secret_expires_in: process.env.JWT_ACCESS_SECRET_EXPIRES_IN,
+  jwt_refresh_secret_exires_in: process.env.JWT_REFRESH_SECRET_EXPIRES_IN,
   sp: {
     sp_endpoint: process.env.SP_ENDPOINT,
     sp_username: process.env.SP_USERNAME,
@@ -21,11 +25,5 @@ export default {
     sp_prefix: process.env.SP_PREFIX,
     sp_return_url: process.env.SP_RETURN_URL,
   },
-  //added by mirza nahid
-  //jwt and refresh secret and expiry added here
-  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
-  jwt_access_secret_expires_in: process.env.JWT_ACCESS_SECRET_EXPIRES_IN,
-  jwt_refresh_secret_exires_in: process.env.JWT_REFRESH_SECRET_EXPIRES_IN,
 }
 
